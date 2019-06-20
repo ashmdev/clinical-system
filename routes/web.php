537cf0cd6e92e08865a8b03b00bcd8a65593582b
ashmdev <AbdelkarinHM@gmail.com>
2019-06-20 03:02:05 -0400

    probando clousure

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes(['verify'=> true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/test', function(){
     return 'Hola desde un closure'; 
 });
